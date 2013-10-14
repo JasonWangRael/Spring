2e2901777ca68c2d359b01b1cd0fb3b3a1860a9f
JasonWangRael.office <jason.sixsteps@qq.com>
2013-10-14 12:07:19 +0800

    Change-Id: I057e7dc63dbadf7f0f695100ebcf83fe334aa3fe Signed-off-by: JasonWangRael.office <jason.sixsteps@qq.com>

---
+++ SpringInActionExample/src/com/springinaction/springidol/test/MainTest.java
@@ -0,0 +1,14 @@
+package com.springinaction.springidol.test;
+
+public class MainTest {
+
+	/**
+	 * @param args
+	 * @description
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
